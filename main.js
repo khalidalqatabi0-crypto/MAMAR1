e5820f8edba3136f0591cf93476c1d3dea1ad18a
khalidalqatabi0-crypto <khalidalqatabi0@gmail.com>
2025-10-22 11:24:58 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -78,3 +78,11 @@ scrollReveal().reveal(".favourite__card ",{
    
     interval:500,
 });
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector("nav");
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
